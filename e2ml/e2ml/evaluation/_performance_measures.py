7eb8d0d6a2882c35cefc53475dc854501634e42b
KlaraGtknst <klara.gutekunst.kg@gmail.com>
2023-06-09 12:07:22 +0200

    added notebook 8 work from course

--- e2ml/e2ml/evaluation/_performance_measures.py
+++ e2ml/e2ml/evaluation/_performance_measures.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from sklearn.utils.validation import check_consistent_length, check_scalar, column_or_1d
 
-from notebooks.evaluation import zero_one_loss
+from . import zero_one_loss
 
 
 def confusion_matrix(y_true, y_pred, *, n_classes=None, normalize=None):
@@ -30,40 +30,44 @@ def confusion_matrix(y_true, y_pred, *, n_classes=None, normalize=None):
         Confusion matrix whose i-th row and j-th column entry indicates the number of samples with true label being
         i-th class and predicted label being j-th class.
     """
-    if len([filter(lambda x: x < 0, set(y_true + y_pred))]) > 0:
-        raise ValueError('Classes should be 0 or bigger')
-
     # create empty matrix
-    n_classes = n_classes if n_classes is not None else max(*y_true, *y_pred) + 1
+    y_true = column_or_1d(y_true, int).astype(int)
+    y_pred = column_or_1d(y_pred, int).astype(int)
+
+    check_consistent_length(y_true, y_pred)
+    #[check_scalar(y, target_type=np.dtype(int), name='y_true') for y in y_true]
+    #[check_scalar(y, target_type=np.dtype(int), name='y_pred') for y in y_pred]
+
+    y_min = np.min((y_true.min(), y_pred.min()))
+    y_max = np.max((y_true.max(), y_pred.max()))
+
+    if y_min < 0:
+        raise ValueError('Negative value')
+
+    if n_classes is None:
+        n_classes = int(y_max + 1)
     C = np.zeros((n_classes, n_classes), dtype=int)
-    print(C)
-    print(y_true)
 
     # c_i,j = true label i, predicted label j
-    for i in range(len(y_true)):
-        real_class = y_true[i]
-        pred_class = y_pred[i]
-        C[real_class, pred_class] += 1
+    for i in range(n_classes):
+        for j in range(n_classes):
+            C[i, j] = np.sum((y_true == i) & (y_pred == j))
 
     # normalize
-    if normalize == 'true':
-        # sum(row) == 1
-        for row in C:
-            for cell in range(len(row)):
-                row[cell] = float(row[cell]/sum(row))
+    with np.errstate(all='ignore'):
+        if normalize == 'true':
+            # sum(row) == 1
+            C = C/C.sum(axis=1, keepdims=True)
 
-    elif normalize == 'pred':
-        # sum(column) == 1
-        for col in C.T:
-            for cell in range(len(col)):
-                col[cell] = float(col[cell]/sum(col))
+        elif normalize == 'pred':
+            # sum(column) == 1
+            C = C/C.sum(axis=0, keepdims=True)
 
-    elif normalize == 'all':
-        # sum(matrix ) == 1
-        for row in C:
-            for cell in range(len(row)):
-                row[cell] = float(row[cell]/np.sum(C))
+        elif normalize == 'all':
+            # sum(matrix ) == 1
+            C = C/C.sum()
 
+        C = np.nan_to_num(C)
     return C
 
 
@@ -83,7 +87,22 @@ def accuracy(y_true, y_pred):
     acc : float in [0, 1]
         Accuracy.
     """
+
+    y_true = column_or_1d(y_true, int).astype(int)
+    y_pred = column_or_1d(y_pred, int).astype(int)
+
+    y_max = np.max((y_true.max(), y_pred.max()))
+    n_classes = y_max + 1
+
     C = confusion_matrix(y_true, y_pred, normalize=None)
+
+    # alternative
+    #return 1 - zero_one_loss(y_true=y_true, y_pred=y_pred)
+
+    #num = 0
+    #dem = 0
+
+    #for i in range()
     return np.trace(C)/np.sum(C)
 
 
@@ -123,7 +142,7 @@ def cohen_kappa(y_true, y_pred, n_classes=None):
     kappa = 1 - np.sum(w_mat * C) / np.sum(w_mat * expected)
     return kappa
 
-
+#TODO
 def macro_f1_measure(y_true, y_pred, n_classes=None):
     """Computes the marco F1 measure.
 
@@ -158,7 +177,7 @@ def macro_f1_measure(y_true, y_pred, n_classes=None):
             # percision: TP/ (TP + FP)
             prec = true_positives / (true_positives + false_positives)
 
-            # recall: TP / (TP + FN)
+            # recall: TP / (TP + FN) = sensitivity, FN = should be positive but is assigned negative
             recall = true_positives / (true_positives + false_negative)
 
             f1_classes[c] = (2* np.cross(prec, recall))/ (prec + recall)
