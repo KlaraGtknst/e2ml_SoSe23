0f6ccf97950a84b240ce86ee8a0446ce5613451f
KlaraGtknst <klara.gutekunst.kg@gmail.com>
2023-06-16 11:46:21 +0200

    content of notebook 9 (incl. lift charts)

--- e2ml/e2ml/evaluation/_performance_measures_II.py
+++ e2ml/e2ml/evaluation/_performance_measures_II.py
@@ -21,7 +21,35 @@ def roc_curve(labels, x, scores):
         at different classification thresholds.
 
     """
-# TODO 
+    x = 1 - x
+    labels = np.array(labels)
+    scores = np.array(scores)
+
+    # tp, fp ermitteln
+    positive_idx = scores[labels == x]
+    negative_idx = scores[labels != x]
+
+    # sortieren nach labels (erste negative, dann positive), dann nach scores (klein nach groß)
+    #np.lexsort(np.vstack((scores, labels)))
+    # iterieren über thresholds
+    zs_idx = np.argsort(scores)
+    zs = scores[zs_idx] # sorted scores
+    ls = labels[zs_idx] # sorted labels
+
+    roc_curve = np.zeros((len(labels), 2))
+    for i,t in enumerate(zs):
+        pred = (zs >= t) # predicted labels
+        tp = (ls == pred) & (ls == x) # true positives: pred==ground truth==positive class
+        fp = (ls != pred) & (ls != x) # false positives: pred!=ground truth==positive class FALSCH
+
+        tpr = sum(tp) / sum(ls == x) # true positive rate
+        fpr = sum(fp) / sum(ls != x) # false positive rate
+
+        roc_curve[i,:] = [tpr, fpr]
+
+    roc_curve[-1,:] = [1,1]
+    return roc_curve
+
 
 def roc_auc(points):
     """
@@ -30,7 +58,7 @@ def roc_auc(points):
     Parameters
     ----------
     points : array-like
-        List of points representing the ROC curve.
+        List of points representing the ROC curve. (FPR und TPR an verschiedenen Thresholds)
 
     Returns
     -------
@@ -38,7 +66,13 @@ def roc_auc(points):
         Area Under the ROC curve.
 
     """
-# TODO 
+    points = np.array(points)
+    tmp = np.vstack(([[0,0]], points[np.lexsort(points.T)], [[1,1]]))
+    x_dif = tmp[1:,0] - tmp[:-1,0]
+    y_dif = tmp[1:,1] + tmp[:-1,1]
+    area = np.sum(x_dif * y_dif/2)
+
+    return area
 
 
 
@@ -60,7 +94,46 @@ def draw_lift_chart(true_labels, pos, predicted):
     None
 
     """
-# TODO 
+    # thresholds sind egal
+    # x-Achse: Anteil der Datenpunkte
+    # y-Achse: Anteil der positiven Klasse
+    plt.figure(figsize=(10, 6))
+    plt.title('Lift Chart')
+    plt.xlabel('Sample size')
+    plt.ylabel('True positives')
+    plt.grid(True)
+
+    # Code aus Übung
+    tp = np.ones((len(true_labels), 2))
+    tp[:,0] = np.cumsum(tp[:,0])
+    for (l,p,i) in zip(true_labels, predicted, np.arange(0, len(true_labels))):
+        tp[i,1] = (l == p) & (l == pos)
+    tp[:,1] = np.cumsum(tp[:,1])
+    plt.plot(tp[:,0], tp[:,1], 'b-')
+    plt.scatter(tp[:,0], tp[:,1])
+    plt.show()
+
+
+    # Mareks Code
+    n = len(true_labels)    # number of samples
+
+    # store sample index and true labels
+    tmp = np.zeros((n, 2))
+    tmp[:,0] = np.arange(1,n+1)
+    tmp[:,1] = np.array(true_labels)
+
+    with np.nditer(tmp[:,1], op_flags=['readwrite'], flags=['f_index']) as it:
+        for x in it:
+            if x == pos and x[...] == predicted[it.index]:
+                x[...] = 1  # tp
+            else:
+                x[...] = 0  # kein tp
+
+    tmp[:,1] = np.cumsum(tmp[:,1]) # kumulierte summe der true labels
+
+    plt.scatter(tmp[:,0], tmp[:,1], s=5, c='b', marker='o')
+    plt.show()
+
 
 def kl_divergence(p, q):
     """
@@ -79,4 +152,10 @@ def kl_divergence(p, q):
         KL divergence between P and Q.
 
     """
-# TODO 
+    p = np.array(p)
+    q = np.array(q)
+
+    log_ratio = np.log(p/q)
+    kl_div = np.sum(p * log_ratio)
+
+    return kl_div
